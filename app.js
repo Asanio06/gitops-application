6eacf4ed29591d93ef3b0b09286de4d47c184df0
Lansana Diomande <lansana.diomande@epfedu.fr>
2022-12-27 19:06:50 +0100

    new version

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello Argo CD new version!'))
+app.get('/', (req, res) => res.send('Hello Argo CD new version 2!'))
 app.listen(3000, () => console.log('Server ready'))
