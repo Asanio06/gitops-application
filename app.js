bf94e9e34a51e3485967b23796ded767b0ad25b5
Lansana Diomande <lansana.diomande@epfedu.fr>
2022-12-30 22:05:44 +0100

    new version

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello Argo CD with github action!' + process.env.MY_TEST_PASSWORD))
+app.get('/', (req, res) => res.send('Hello Argo CD with github action!' + process.env.MY_TEST_PASSWORD + 'password'))
 app.listen(3000, () => console.log('Server ready'))
